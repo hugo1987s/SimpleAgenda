673d1310debda1df87f74c42bcc913c520b4972b
Carlos Almirón <carlosalmiron@openmailbox.org>
2018-03-17 13:36:46 -0300

    Se agrega Localidad a modelo de la agenda

--- src/modelo/Agenda.java
+++ src/modelo/Agenda.java
@@ -1,33 +1,42 @@
 package modelo;
 
 import java.util.List;
+
+import dto.LocalidadDTO;
 import dto.PersonaDTO;
 import persistencia.dao.interfaz.DAOAbstractFactory;
+import persistencia.dao.interfaz.LocalidadDAO;
 import persistencia.dao.interfaz.PersonaDAO;
 
-
-public class Agenda 
+public class Agenda
 {
-	private PersonaDAO persona;	
-	
+	private PersonaDAO persona;
+	private LocalidadDAO localidad;
+
 	public Agenda(DAOAbstractFactory metodo_persistencia)
 	{
 		this.persona = metodo_persistencia.createPersonaDAO();
+		this.localidad = metodo_persistencia.createLocalidadDAO();
 	}
-	
+
 	public void agregarPersona(PersonaDTO nuevaPersona)
 	{
 		this.persona.insert(nuevaPersona);
 	}
 
-	public void borrarPersona(PersonaDTO persona_a_eliminar) 
+	public void borrarPersona(PersonaDTO persona_a_eliminar)
 	{
 		this.persona.delete(persona_a_eliminar);
 	}
-	
+
 	public List<PersonaDTO> obtenerPersonas()
 	{
-		return this.persona.readAll();		
+		return this.persona.readAll();
 	}
-	
+
+	public List<LocalidadDTO> obtenerLocalidades()
+	{
+		return this.localidad.readAll();
+	}
+
 }
