617619e3c3f78550e1f947057d2c5dee8e2ca9c8
Carlos Almirón <carlosalmiron@openmailbox.org>
2018-03-17 01:52:06 -0300

    Se agrega combo de Localidades en ventana de Persona

--- src/presentacion/vista/VentanaPersona.java
+++ src/presentacion/vista/VentanaPersona.java
@@ -1,7 +1,7 @@
 package presentacion.vista;
 
-
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,72 +10,80 @@ import javax.swing.border.EmptyBorder;
 
 import presentacion.controlador.Controlador;
 
-public class VentanaPersona extends JFrame 
+public class VentanaPersona extends JFrame
 {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtNombre;
 	private JTextField txtTelefono;
+	private JComboBox<String> cboLocalidad;
 	private JButton btnAgregarPersona;
 	private Controlador controlador;
 
-	public VentanaPersona(Controlador controlador) 
+	public VentanaPersona(Controlador controlador)
 	{
 		super();
 		this.controlador = controlador;
-		
+
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 343, 183);
+		setBounds(100, 100, 343, 227);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 307, 123);
+		panel.setBounds(10, 11, 307, 164);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblNombreYApellido = new JLabel("Nombre y apellido");
 		lblNombreYApellido.setBounds(10, 11, 113, 14);
 		panel.add(lblNombreYApellido);
-		
+
 		JLabel lblTelfono = new JLabel("Tel\u00E9fono");
 		lblTelfono.setBounds(10, 52, 113, 14);
 		panel.add(lblTelfono);
-		
+
+		JLabel lblLocalidad = new JLabel("Localidad");
+		lblLocalidad.setBounds(10, 93, 113, 14);
+		panel.add(lblLocalidad);
+
 		txtNombre = new JTextField();
 		txtNombre.setBounds(133, 8, 164, 20);
 		panel.add(txtNombre);
 		txtNombre.setColumns(10);
-		
+
 		txtTelefono = new JTextField();
 		txtTelefono.setBounds(133, 49, 164, 20);
 		panel.add(txtTelefono);
 		txtTelefono.setColumns(10);
-		
+
+		cboLocalidad = new JComboBox<String>(new String[] {});
+		cboLocalidad.setBounds(133, 90, 164, 20);
+		panel.add(cboLocalidad);
+
 		btnAgregarPersona = new JButton("Agregar");
 		btnAgregarPersona.addActionListener(this.controlador);
-		btnAgregarPersona.setBounds(208, 92, 89, 23);
+		btnAgregarPersona.setBounds(208, 133, 89, 23);
 		panel.add(btnAgregarPersona);
-		
+
 		this.setVisible(true);
 	}
-	
-	public JTextField getTxtNombre() 
+
+	public JTextField getTxtNombre()
 	{
 		return txtNombre;
 	}
 
-	public JTextField getTxtTelefono() 
+	public JTextField getTxtTelefono()
 	{
 		return txtTelefono;
 	}
 
-	public JButton getBtnAgregarPersona() 
+	public JButton getBtnAgregarPersona()
 	{
 		return btnAgregarPersona;
 	}
-	
-}
 
+}
