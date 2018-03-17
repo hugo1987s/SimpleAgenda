ba718213b64454ac86fa7752abb6c842a65e95b0
Hugo <asdfas>
2018-03-17 00:20:13 -0300

    Se agregan los campos faltantes al formulario de Alta/Modificacion de Persona.

--- src/presentacion/vista/VentanaPersona.java
+++ src/presentacion/vista/VentanaPersona.java
@@ -9,6 +9,7 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import presentacion.controlador.Controlador;
+import javax.swing.JComboBox;
 
 public class VentanaPersona extends JFrame 
 {
@@ -18,21 +19,28 @@ public class VentanaPersona extends JFrame
 	private JTextField txtTelefono;
 	private JButton btnAgregarPersona;
 	private Controlador controlador;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	public VentanaPersona(Controlador controlador) 
 	{
 		super();
+		setResizable(false);
 		this.controlador = controlador;
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 343, 183);
+		setBounds(100, 100, 466, 435);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 307, 123);
+		panel.setBounds(10, 11, 442, 381);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -41,23 +49,97 @@ public class VentanaPersona extends JFrame
 		panel.add(lblNombreYApellido);
 		
 		JLabel lblTelfono = new JLabel("Tel\u00E9fono");
-		lblTelfono.setBounds(10, 52, 113, 14);
+		lblTelfono.setBounds(10, 44, 113, 14);
 		panel.add(lblTelfono);
 		
 		txtNombre = new JTextField();
-		txtNombre.setBounds(133, 8, 164, 20);
+		txtNombre.setBounds(133, 8, 297, 20);
 		panel.add(txtNombre);
 		txtNombre.setColumns(10);
 		
 		txtTelefono = new JTextField();
-		txtTelefono.setBounds(133, 49, 164, 20);
+		txtTelefono.setBounds(133, 41, 164, 20);
 		panel.add(txtTelefono);
 		txtTelefono.setColumns(10);
 		
 		btnAgregarPersona = new JButton("Agregar");
 		btnAgregarPersona.addActionListener(this.controlador);
-		btnAgregarPersona.setBounds(208, 92, 89, 23);
+		btnAgregarPersona.setBounds(243, 350, 89, 23);
 		panel.add(btnAgregarPersona);
+		
+		JLabel lblCalle = new JLabel("Calle");
+		lblCalle.setBounds(10, 77, 113, 14);
+		panel.add(lblCalle);
+		
+		JLabel lblAltura = new JLabel("Altura");
+		lblAltura.setBounds(10, 110, 113, 14);
+		panel.add(lblAltura);
+		
+		JLabel lblPiso = new JLabel("Piso");
+		lblPiso.setBounds(10, 142, 113, 14);
+		panel.add(lblPiso);
+		
+		JLabel lblDepartamento = new JLabel("Departamento");
+		lblDepartamento.setBounds(10, 175, 113, 14);
+		panel.add(lblDepartamento);
+		
+		JLabel lblLocalidad = new JLabel("Localidad");
+		lblLocalidad.setBounds(10, 209, 113, 14);
+		panel.add(lblLocalidad);
+		
+		JLabel lblFechaDeCumpleaos = new JLabel("Fecha de Cumplea\u00F1os");
+		lblFechaDeCumpleaos.setBounds(12, 276, 149, 14);
+		panel.add(lblFechaDeCumpleaos);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setBounds(10, 243, 113, 14);
+		panel.add(lblEmail);
+		
+		JLabel lblTipoDeContacto = new JLabel("Tipo de Contacto");
+		lblTipoDeContacto.setBounds(10, 307, 149, 14);
+		panel.add(lblTipoDeContacto);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(133, 107, 67, 20);
+		panel.add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(133, 74, 297, 20);
+		panel.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(133, 172, 67, 20);
+		panel.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(133, 139, 67, 20);
+		panel.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(168, 273, 164, 20);
+		panel.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(133, 240, 297, 20);
+		panel.add(textField_5);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(133, 205, 297, 22);
+		panel.add(comboBox);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(133, 303, 199, 22);
+		panel.add(comboBox_1);
+		
+		JButton btnCerrar = new JButton("Cerrar");
+		btnCerrar.setBounds(341, 350, 89, 23);
+		panel.add(btnCerrar);
 		
 		this.setVisible(true);
 	}
@@ -76,6 +158,5 @@ public class VentanaPersona extends JFrame
 	{
 		return btnAgregarPersona;
 	}
-	
 }
 
