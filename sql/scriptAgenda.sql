d91a18e9c7aac8960a89300d1e773f126ee7901d
Carlos Almirón <carlosalmiron@openmailbox.org>
2018-03-17 11:04:25 -0300

    Se agrega tabla localidades a la base

--- sql/scriptAgenda.sql
+++ sql/scriptAgenda.sql
@@ -1,9 +1,21 @@
 CREATE DATABASE `agenda`;
 USE agenda;
+
+DROP TABLE IF EXISTS `localidades`;
+DROP TABLE IF EXISTS `personas`;
+
+CREATE TABLE `localidades` 
+(
+  `codigoPostal` int(5) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`codigoPostal`)
+);
 CREATE TABLE `personas` 
 (
   `idPersona` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(45) NOT NULL,
   `Telefono` varchar(20) NOT NULL,
-  PRIMARY KEY (`idPersona`)
+  `localidad` int(5),
+  PRIMARY KEY (`idPersona`),
+  FOREIGN KEY (`localidad`) REFERENCES `localidades`(`codigoPostal`)
 );
