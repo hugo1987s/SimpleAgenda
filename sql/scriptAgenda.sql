0b0bc75bf0493e78c8709285be915f1913a1f8bf
Hugo <asdfas>
2018-03-17 10:49:34 -0300

    Se agregan campos a la tabla Personas. Se crean las tablas Localidades y TipoContacto.

--- sql/scriptAgenda.sql
+++ sql/scriptAgenda.sql
@@ -1,9 +1,33 @@
-CREATE DATABASE `agenda`;
+CREATE DATABASE Agenda;
 USE agenda;
-CREATE TABLE `personas` 
+CREATE TABLE Personas
 (
-  `idPersona` int(11) NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) NOT NULL,
-  `Telefono` varchar(20) NOT NULL,
-  PRIMARY KEY (`idPersona`)
+  IdPersona int(11) NOT NULL AUTO_INCREMENT,
+  Nombre varchar(45) NOT NULL,
+  Telefono varchar(20) NOT NULL,
+  Calle varchar(50),
+  Altura int,
+  Piso int,
+  Departamento varchar(10),
+  CodigoPostal int(5),
+  Email varchar(100),
+  FechaNacimiento date,
+  IdTipoContacto int,
+  PRIMARY KEY (IdPersona)
+);
+
+
+CREATE TABLE Localidades 
+(
+  CodigoPostal int(5) NOT NULL,
+  Nombre varchar(45) NOT NULL,
+  PRIMARY KEY (CodigoPostal)
+);
+
+
+CREATE TABLE TipoContacto
+(
+  IdTipoContacto int NOT NULL AUTO_INCREMENT,
+  Tipo varchar(45) NOT NULL,
+  PRIMARY KEY (IdTipoContacto)
 );
