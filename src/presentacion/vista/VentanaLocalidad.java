20171de2873c59f86d7af741cfb39434611213ea
Carlos Almirón <carlosalmiron@openmailbox.org>
2018-03-17 11:54:01 -0300

    Se agrega tabla en ABM localidades

--- src/presentacion/vista/VentanaLocalidad.java
+++ src/presentacion/vista/VentanaLocalidad.java
@@ -2,7 +2,10 @@ package presentacion.vista;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 import presentacion.controlador.Controlador;
 
@@ -10,6 +13,9 @@ public class VentanaLocalidad extends JFrame
 {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable tablaLocalidades;
+	private DefaultTableModel modelLocalidades;
+	private String[] nombreColumnas = {"Nombre", "Codigo Postal"};
 	private Controlador controlador;
 
 	public VentanaLocalidad(Controlador controlador)
@@ -28,7 +34,21 @@ public class VentanaLocalidad extends JFrame
 		panel.setBounds(10, 11, 327, 164);
 		contentPane.add(panel);
 		panel.setLayout(null);
+		
+		JScrollPane spLocalidades = new JScrollPane();
+		spLocalidades.setBounds(10, 11, 307, 147);
+		panel.add(spLocalidades);
+		
+		modelLocalidades = new DefaultTableModel(null, nombreColumnas);
+		tablaLocalidades = new JTable(modelLocalidades);
+		
+		tablaLocalidades.getColumnModel().getColumn(0).setPreferredWidth(103);
+		tablaLocalidades.getColumnModel().getColumn(0).setResizable(false);
+		tablaLocalidades.getColumnModel().getColumn(1).setPreferredWidth(100);
+		tablaLocalidades.getColumnModel().getColumn(1).setResizable(false);
 
+		spLocalidades.setViewportView(tablaLocalidades);
+		
 		this.setVisible(true);
 	}
 
