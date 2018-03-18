f74a7f087c22bc3d0e0fe2d6b46994bc218395a6
Carlos Almirón <carlosalmiron@openmailbox.org>
2018-03-17 22:33:48 -0300

    Se agregan campos en el ABM para la edicion de localidades

--- src/presentacion/vista/VentanaLocalidad.java
+++ src/presentacion/vista/VentanaLocalidad.java
@@ -2,9 +2,11 @@ package presentacion.vista;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 import presentacion.controlador.Controlador;
@@ -20,6 +22,10 @@ public class VentanaLocalidad extends JFrame
 	private DefaultTableModel modelLocalidades;
 	private String[] nombreColumnas = {"Nombre", "Codigo Postal"};
 	private Controlador controlador;
+	private JTextField txtNombreLocalidad;
+	private JLabel lblNombreLocalidad;
+	private JLabel lblCodigoPostal;
+	private JTextField txtCodigoPostal;
 
 	public VentanaLocalidad(Controlador controlador)
 	{
@@ -27,13 +33,13 @@ public class VentanaLocalidad extends JFrame
 		this.controlador = controlador;
 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 365);
 		contentPane = new JPanel();
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 434, 262);
+		panel.setBounds(0, 0, 434, 327);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -51,16 +57,34 @@ public class VentanaLocalidad extends JFrame
 
 		spLocalidades.setViewportView(tablaLocalidades);
 		
+		lblNombreLocalidad = new JLabel("Nombre");
+		lblNombreLocalidad.setBounds(10, 220, 113, 14);
+		panel.add(lblNombreLocalidad);
+
+		txtNombreLocalidad = new JTextField();
+		txtNombreLocalidad.setBounds(133, 217, 164, 20);
+		txtNombreLocalidad.setColumns(10);
+		panel.add(txtNombreLocalidad);
+		
+		lblCodigoPostal = new JLabel("Codigo Postal");
+		lblCodigoPostal.setBounds(10, 261, 113, 14);
+		panel.add(lblCodigoPostal);
+
+		txtCodigoPostal = new JTextField();
+		txtCodigoPostal.setBounds(133, 258, 164, 20);
+		txtCodigoPostal.setColumns(10);
+		panel.add(txtCodigoPostal);
+		
 		btnAgregar = new JButton("Agregar");
-		btnAgregar.setBounds(10, 228, 89, 23);
+		btnAgregar.setBounds(10, 293, 89, 23);
 		panel.add(btnAgregar);
 		
 		btnEditar = new JButton("Editar");
-		btnEditar.setBounds(109, 228, 89, 23);
+		btnEditar.setBounds(109, 293, 89, 23);
 		panel.add(btnEditar);
 		
 		btnBorrar = new JButton("Borrar");
-		btnBorrar.setBounds(208, 228, 89, 23);
+		btnBorrar.setBounds(208, 293, 89, 23);
 		panel.add(btnBorrar);
 		
 		this.setVisible(true);
