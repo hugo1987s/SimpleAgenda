fffdf546b9f2be04eb12a42b8c40dbe277ec60d1
Carlos Almirón <carlosalmiron@openmailbox.org>
2018-03-22 02:39:49 -0300

    Se agregan comillas al sql

--- sql/scriptAgenda.sql
+++ sql/scriptAgenda.sql
@@ -22,19 +22,19 @@ CREATE TABLE `tipoContacto`
 
 CREATE TABLE `personas` 
 (
-  IdPersona int(11) NOT NULL AUTO_INCREMENT,
-  Nombre varchar(45) NOT NULL,
-  Apellido varchar(45) NOT NULL,
-  Telefono varchar(20) NOT NULL,
-  Calle varchar(50),
-  Altura int,
-  Piso int,
-  Departamento varchar(10),
-  CodigoPostal int(5),
-  Email varchar(100),
-  FechaNacimiento date,
-  IdTipoContacto int,
-  PRIMARY KEY (IdPersona),
+  `IdPersona` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) NOT NULL,
+  `Apellido` varchar(45) NOT NULL,
+  `Telefono` varchar(20) NOT NULL,
+  `Calle` varchar(50),
+  `Altura` int,
+  `Piso` int,
+  `Departamento` varchar(10),
+  `CodigoPostal` int(5),
+  `Email` varchar(100),
+  `FechaNacimiento` date,
+  `IdTipoContacto` int,
+  PRIMARY KEY (`IdPersona`),
   FOREIGN KEY (`CodigoPostal`) REFERENCES `localidades`(`CodigoPostal`),
   FOREIGN KEY (`IdTipoContacto`) REFERENCES `tipoContacto`(`IdTipoContacto`)
 );
